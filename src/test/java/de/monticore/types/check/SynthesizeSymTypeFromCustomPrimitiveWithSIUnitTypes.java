daf4877f3d80a3ea1d029520f0c8827688a9ce7b
Malte Heithoff <Malte.Heithoff@rwth-aachen.de>
2020-05-19 20:02:46 +0200

    Updated TypeCheck classes to match the dev-changes. Mainly removed the scope and only work with nodes' enclosing scopes + removed prettyprinter

--- src/test/java/de/monticore/types/check/SynthesizeSymTypeFromCustomPrimitiveWithSIUnitTypes.java
+++ src/test/java/de/monticore/types/check/SynthesizeSymTypeFromCustomPrimitiveWithSIUnitTypes.java
@@ -1,32 +1,17 @@
 package de.monticore.types.check;
 
-import de.monticore.expressions.expressionsbasis._symboltable.IExpressionsBasisScope;
 import de.monticore.lang.types.customprimitivewithsiunittypes._ast.ASTCustomPrimitiveWithSIUnitType;
 import de.monticore.lang.types.customprimitivewithsiunittypes._visitor.CustomPrimitiveWithSIUnitTypesVisitor;
-import de.monticore.lang.types.siunittypes._ast.ASTSIUnitType;
-import de.se_rwth.commons.logging.Log;
 
 /**
  * Visitor for Derivation of SymType from PrimitiveWithSIUnitTypes
  * i.e. for
  * types/PrimitiveWithSIUnitTypes.mc4
  */
-public class SynthesizeSymTypeFromCustomPrimitiveWithSIUnitTypes extends SynthesizeSymTypeFromMCBasicTypes
+public class SynthesizeSymTypeFromCustomPrimitiveWithSIUnitTypes extends SynthesizeSymTypeFromPrimitiveWithSIUnitTypes
         implements CustomPrimitiveWithSIUnitTypesVisitor {
-    /**
-     * Using the visitor functionality to calculate the SymType Expression
-     */
 
-    // ----------------------------------------------------------  realThis start
-    // setRealThis, getRealThis are necessary to make the visitor compositional
-    //
-    // (the Vistors are then composed using theRealThis Pattern)
-    //
     CustomPrimitiveWithSIUnitTypesVisitor realThis = this;
-
-    public SynthesizeSymTypeFromCustomPrimitiveWithSIUnitTypes(IExpressionsBasisScope scope) {
-        super(scope);
-    }
 
     @Override
     public void setRealThis(CustomPrimitiveWithSIUnitTypesVisitor realThis) {
@@ -39,32 +24,7 @@ public class SynthesizeSymTypeFromCustomPrimitiveWithSIUnitTypes extends Synthes
     }
 
     @Override
-    public void endVisit(ASTSIUnitType node) {
-        lastResult.setLast(SIUnitSymTypeExpressionFactory.createSIUnit(node.toString(), scope));
-    }
-
-    @Override
     public void traverse(ASTCustomPrimitiveWithSIUnitType node) {
-        SymTypeExpression primitiveType = null;
-        SymTypeExpression siunitType = null;
-        node.getMCPrimitiveType().accept(getRealThis());
-        if (!lastResult.isPresentLast() || !isNumericType(lastResult.getLast())) {
-            Log.error("0x"); // TODO
-        }
-        primitiveType = lastResult.getLast();
-        lastResult.reset();
-        node.getSIUnitType().accept(getRealThis());
-        if (!lastResult.isPresentLast()){
-            Log.error("0x"); // TODO
-        }
-        siunitType = lastResult.getLast();
-        lastResult.setLast(SIUnitSymTypeExpressionFactory.createNumericWithSIUnitType((SymTypeConstant) primitiveType, siunitType, scope));
-    }
-
-    /**
-     * test if the expression is of numeric type (double, float, long, int, char, short, byte)
-     */
-    private static boolean isNumericType(SymTypeExpression type) {
-        return type.isTypeConstant() && ((SymTypeConstant) type).isNumericType();
+        traversePrimitiveWithSIUnitType(node);
     }
 }
