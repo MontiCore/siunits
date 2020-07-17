498343ab09d6908bc40de8b256eeedabea8ab7ba
Malte Heithoff <Malte.Heithoff@rwth-aachen.de>
2020-07-17 16:23:21 +0200

    Update GeneratorTest

--- src/test/java/de/monticore/lang/testsijava/testsijava/generator/GeneratorTest.java
+++ src/test/java/de/monticore/lang/testsijava/testsijava/generator/GeneratorTest.java
@@ -1,16 +1,26 @@
 package de.monticore.lang.testsijava.testsijava.generator;
 
 import de.se_rwth.commons.logging.Log;
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class GeneratorTest {
 
     private String modelPath = "src/test/resources";
     private String outputPath = "target/generated-test-sources/monticore/sourcecode";
+    private String compareModelPath = "src/test/resources/compare";
 
-    private void test(String model) {
+    private void test(String model) throws IOException {
         Generator.generate(modelPath, model, outputPath);
+        String javaModelName = model.replace(".sijava", ".java");
+        File generatedFile = new File(outputPath + "/" + javaModelName);
+        File compareFile = new File(compareModelPath + "/" + javaModelName);
+        assert (generatedFile.exists());
+        assert (FileUtils.contentEquals(compareFile, generatedFile));
     }
 
     @Before
@@ -19,13 +29,13 @@ public class GeneratorTest {
     }
 
     @Test
-    public void testMyClass() {
+    public void testMyClass() throws IOException {
         String model = "test/de/monticore/lang/testsijava/testsijava/MyClass.sijava";
         test(model);
     }
 
     @Test
-    public void testMain() {
+    public void testMain() throws IOException {
         String model = "test/de/monticore/lang/testsijava/testsijava/Main.sijava";
         test(model);
     }
