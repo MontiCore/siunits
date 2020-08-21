c19182fab98d448bdea26989e7ed4beb6acef2f4
Malte Heithoff <Malte.Heithoff@rwth-aachen.de>
2020-08-21 11:40:01 +0200

    Edited test files

--- src/test/resources/compare/test/de/monticore/lang/testsijava/testsijava/TestPrintAndValue.java
+++ src/test/resources/compare/test/de/monticore/lang/testsijava/testsijava/TestPrintAndValue.java
@@ -5,13 +5,15 @@ package test.de.monticore.lang.testsijava.testsijava;
 public class TestPrintAndValue {
 
     public void main() {
-        System.out.println(((20.0) * 1000.0) + 30.0 + "m");
         double var = (double) ((7200.0) / 3600.0);
+        System.out.println(((20.0) * 1000.0) + 30.0 + "m");
         System.out.println(var + "h");
         System.out.println(var);
         System.out.println(var + 10);
+        System.out.println(((var) * 60.0) + 30.0);
         System.out.println(((var) * 3600.0));
         System.out.println(((var) * 3600.0) + 10);
+        System.out.println(((((var) * 60.0) + 30.0) * 60.0));
         System.out.println(2000.0 / var + "km/h");
         System.out.println(((var) * 3600000.0) + 10.0 + "ms");
     }
