6afd2fae0c3cda6ecaafdd8a4ad4b3afab767a6f
Malte Heithoff <h.malte.f@googlemail.com>
2020-09-22 11:38:30 +0200

    TypeCheckCoCo is more general now

---
+++ src/main/java/de/monticore/types/check/cocos/TypeCheckCoCo.java
@@ -0,0 +1,139 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.types.check.cocos;
+
+import de.monticore.ast.ASTNode;
+import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
+import de.monticore.symbols.basicsymbols._ast.ASTFunction;
+import de.monticore.symbols.basicsymbols._ast.ASTVariable;
+import de.monticore.symbols.oosymbols._ast.ASTField;
+import de.monticore.symbols.oosymbols._ast.ASTMethod;
+import de.monticore.types.check.ISynthesize;
+import de.monticore.types.check.ITypesCalculator;
+import de.monticore.types.check.SymTypeExpression;
+import de.monticore.types.check.TypeCheck;
+import de.se_rwth.commons.logging.Log;
+
+/**
+ * Provides methods to check for incompatible type in assignment expressions,
+ *  Field or Variable declarations, and Method or Function declarations.
+ * Take note, that this CoCo requires the SymbolTableCreator to set the following:
+ *  1. The enclosing scope of each Expression,
+ *   Literal, SignedLiteral, MCType and MCReturnType
+ *  2. The {@link SymTypeExpression} of each VariableSymbol, FieldSymbol, FunctionSymbol, and MethodSymbol
+ */
+public abstract class TypeCheckCoCo {
+
+    protected TypeCheck tc;
+
+    /**
+     * Creates an instance of TypeCheckCoCo
+     * @param typeCheck a {@link TypeCheck} class instantiated with the correct
+     *                  {@link ISynthesize} and {@link ITypesCalculator} classes of
+     *                  the current language
+     */
+    public TypeCheckCoCo(TypeCheck typeCheck) {
+        this.tc = typeCheck;
+    }
+
+    /**
+     * Checks whether the type of the {@link ASTExpression} can be calculated
+     * @param node the {@link ASTExpression} to check
+     */
+    protected void checkExpression(ASTExpression node) {
+        try {
+            // should throw an exception if there are incompatible types
+            SymTypeExpression symTypeExpression = tc.typeOf(node);
+            if (symTypeExpression == null) logError(node, "Incompatible types in AssignmentExpression");
+        } catch (Exception e) {
+            logError(node, "Incompatible types");
+        }
+    }
+
+    /**
+     * Checks whether a {@link ASTVariable} or {@link ASTField} has a type and whether
+     *  an assigned expression has a compatible type
+     * @param node the {@link ASTVariable} or {@link ASTField} to check
+     * @param assignmentExpression the assigned {@link ASTExpression}, can be null if none is available
+     */
+    protected void checkFieldOrVariable(ASTVariable node, ASTExpression assignmentExpression) {
+        if (assignmentExpression == null)
+            checkFieldOrVariable(node, (SymTypeExpression) null);
+        else {
+            SymTypeExpression returnType = null;
+            try {
+                returnType = tc.typeOf(assignmentExpression);
+                if (returnType == null)
+                    logError(assignmentExpression, "Could not calculate type of assigned expression");
+            } catch (Exception e) {
+                logError(assignmentExpression, "Could not calculate type of assigned expression");
+            }
+            if (returnType != null)
+                checkFieldOrVariable(node, returnType);
+        }
+    }
+
+    /**
+     * Checks whether a {@link ASTVariable} or {@link ASTField} has a type and whether an assigned type is compatible
+     * @param node the {@link ASTVariable} or {@link ASTField} to check
+     * @param assignmentType the assigned {@link SymTypeExpression}, can be null if none is available
+     */
+    protected void checkFieldOrVariable(ASTVariable node, SymTypeExpression assignmentType) {
+        if (!node.isPresentSymbol())
+            logError(node, "Variable symbol not present");
+        else if (node.getSymbol().getType() == null)
+            logError(node, "Variable symbol has no type");
+        else if (assignmentType != null && !TypeCheck.compatible(node.getSymbol().getType(), assignmentType))
+            logError(node, String.format("Variable type %s incompatible to assigned type %s",
+                    node.getSymbol().getType().print(), assignmentType.print()));
+    }
+
+    /**
+     * Checks whether a {@link ASTFunction} or {@link ASTMethod} has a type and whether
+     *  a return expression has a compatible type
+     * @param node the {@link ASTFunction} or {@link ASTMethod} to check
+     * @param returnExpression the return {@link ASTExpression}, can be null if none is available
+     */
+    protected void checkMethodOrFunction(ASTFunction node, ASTExpression returnExpression) {
+        if (returnExpression == null)
+            checkMethodOrFunction(node, (SymTypeExpression) null);
+        else {
+            SymTypeExpression returnType = null;
+            try {
+                returnType = tc.typeOf(returnExpression);
+                if (returnType == null)
+                    logError(returnExpression, "Could not calculate type of return expression");
+            } catch (Exception e) {
+                logError(returnExpression, "Could not calculate type of return expression");
+            }
+            if (returnType != null)
+                checkMethodOrFunction(node, returnType);
+        }
+    }
+
+    /**
+     * Checks whether a {@link ASTFunction} or {@link ASTMethod} has a type and whether a return type is compatible
+     * @param node the {@link ASTFunction} or {@link ASTMethod} to check
+     * @param returnType the return {@link SymTypeExpression}, can be null if none is available
+     */
+    protected void checkMethodOrFunction(ASTFunction node, SymTypeExpression returnType) {
+        if (!node.isPresentSymbol())
+            logError(node, "Function symbol not present");
+        else if (node.getSymbol().getReturnType() == null)
+            logError(node, "Function symbol has no return type");
+        else {
+            if (node.getSymbol().getReturnType().isVoidType()) {
+                if (returnType != null && !returnType.isVoidType())
+                    logError(node, String.format("Return type void incompatible to actual return type %s",
+                            returnType.print()));
+            } else if (returnType == null)
+                logError(node, "No return type given");
+            else if (!TypeCheck.compatible(node.getSymbol().getReturnType(), returnType))
+                logError(node, String.format("Return type %s incompatible to actual return type %s",
+                        node.getSymbol().getReturnType().print(), returnType.print()));
+        }
+    }
+
+    protected void logError(ASTNode node, String msg) {
+        Log.error("0xE3810245 " + node.get_SourcePositionStart() + " " + msg);
+    }
+}
---
+++ src/test/java/de/monticore/lang/testsijava/testsijava/_cocos/TestSIJavaTypeCheckCoCo.java
@@ -0,0 +1,58 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+package de.monticore.lang.testsijava.testsijava._cocos;
+
+import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
+import de.monticore.lang.testsijava.testsijava._ast.ASTFieldDeclaration;
+import de.monticore.lang.testsijava.testsijava._ast.ASTMethodDeclaration;
+import de.monticore.lang.testsijava.testsijava._ast.ASTSIJavaClass;
+import de.monticore.lang.testsijava.testsijava._ast.ASTSIJavaMethodExpression;
+import de.monticore.lang.testsijava.testsijava._symboltable.TestSIJavaSymbolTableCreator;
+import de.monticore.lang.testsijava.testsijava._visitor.TestSIJavaVisitor;
+import de.monticore.types.check.DeriveSymTypeOfTestSIJava;
+import de.monticore.types.check.SynthesizeSymTypeFromTestSIJava;
+import de.monticore.types.check.TypeCheck;
+import de.monticore.types.check.cocos.TypeCheckCoCo;
+
+/**
+ * Checks for incompatible types in method expressions and other expressions.
+ * Take note, that this CoCo requires the TestSIJavaSymbolTableCreator to set the type for each
+ *  field and method declaration when building the symbol table and also set the enclosing scope for each
+ *  expression
+ *  @see TestSIJavaSymbolTableCreator
+ */
+public class TestSIJavaTypeCheckCoCo extends TypeCheckCoCo
+    implements TestSIJavaASTSIJavaClassCoCo, TestSIJavaVisitor{
+
+    public static TestSIJavaTypeCheckCoCo getCoCo() {
+        TypeCheck typeCheck = new TypeCheck(new SynthesizeSymTypeFromTestSIJava(), new DeriveSymTypeOfTestSIJava());
+        return new TestSIJavaTypeCheckCoCo(typeCheck);
+    }
+
+    public TestSIJavaTypeCheckCoCo(TypeCheck tc) {
+        super(tc);
+    }
+
+    @Override
+    public void check(ASTSIJavaClass node) {
+        node.accept(this);
+    }
+
+    @Override
+    public void visit(ASTFieldDeclaration node) {
+        checkFieldOrVariable(node, node.getExpression());
+    }
+
+    @Override
+    public void visit(ASTSIJavaMethodExpression node) {
+        checkExpression(node.getExpression());
+    }
+
+    @Override
+    public void visit(ASTMethodDeclaration node) {
+        ASTExpression returnExpression = node.isPresentSIJavaMethodReturn() ?
+                node.getSIJavaMethodReturn().getExpression() : null;
+        checkMethodOrFunction(node, returnExpression);
+    }
+
+}
