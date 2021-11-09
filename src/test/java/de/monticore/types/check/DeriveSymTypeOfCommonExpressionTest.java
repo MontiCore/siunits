2d5c802e9225b92eabdab664cff13f679564a652
breuer <breuer@se-rwth.de>
2021-11-09 11:18:21 +0100

    update errorcodes

--- src/test/java/de/monticore/types/check/DeriveSymTypeOfCommonExpressionTest.java
+++ src/test/java/de/monticore/types/check/DeriveSymTypeOfCommonExpressionTest.java
@@ -632,7 +632,7 @@ public class DeriveSymTypeOfCommonExpressionTest extends DeriveSymTypeAbstractTe
   public void testInvalidCallExpression() throws IOException {
     //method isNot() is not in scope -> method cannot be resolved -> method has no return type
     init_advanced();
-    checkError("isNot()", "0xA0240");
+    checkError("isNot()", "0xA1242");
   }
 
   /**
@@ -1297,7 +1297,7 @@ public class DeriveSymTypeOfCommonExpressionTest extends DeriveSymTypeAbstractTe
   public void testInvalidStaticType() throws IOException {
     init_static_example();
 
-    checkError("B.D", "0xA0303");
+    checkError("B.D", "0xA1303");
   }
 
   @Test
@@ -1311,7 +1311,7 @@ public class DeriveSymTypeOfCommonExpressionTest extends DeriveSymTypeAbstractTe
   public void testInvalidStaticField() throws IOException {
     init_static_example();
 
-    checkError("B.field", "0xA0237");
+    checkError("B.field", "0xA1236");
   }
 
   @Test
@@ -1325,21 +1325,21 @@ public class DeriveSymTypeOfCommonExpressionTest extends DeriveSymTypeAbstractTe
   public void testInvalidStaticMethod() throws IOException {
     init_static_example();
 
-    checkError("B.test()", "0xA0239");
+    checkError("B.test()", "0xA2239");
   }
 
   @Test
   public void testSubClassesDoNotKnowStaticMethodsOfSuperClasses() throws IOException{
     init_static_example();
 
-    checkError("C.test()", "0xA0239");
+    checkError("C.test()", "0xA2239");
   }
 
   @Test
   public void testSubClassesDoNotKnowStaticFieldsOfSuperClasses() throws IOException{
     init_static_example();
 
-    checkError("C.field", "0xA0306");
+    checkError("C.field", "0xA1317");
   }
 
   @Test
