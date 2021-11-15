994e487a645e4a2f934318cbd8e7ab437af8d69b
Jonas Böcker <jonas.boecker@gmx.net>
2021-11-15 09:47:46 +0100

    use java parser in test

--- src/test/java/de/monticore/lang/testsijava/testsijava/generator/PrintAsJavaClassTest.java
+++ src/test/java/de/monticore/lang/testsijava/testsijava/generator/PrintAsJavaClassTest.java
@@ -1,17 +1,23 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.lang.testsijava.testsijava.generator;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ParserConfiguration;
 import de.monticore.lang.testsijava.testsijava.TestSIJavaMill;
 import de.monticore.lang.testsijava.testsijava._ast.ASTSIJavaClass;
 import de.monticore.lang.testsijava.testsijava._parser.TestSIJavaParser;
+import de.monticore.lang.testsijava.testsijava._symboltable.TestSIJavaScopesGenitorDelegator;
 import de.monticore.siunits.SIUnitsMill;
 import de.se_rwth.commons.logging.Log;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Optional;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PrintAsJavaClassTest {
@@ -26,33 +32,44 @@ public class PrintAsJavaClassTest {
     }
 
     @Test
-    public void test1() {
-        String model = "test/de/monticore/lang/testsijava/testsijava/MyClass.sijava";
-        printAsJavaClass(model);
+    public void test1() throws IOException {
+        String model = "src/test/resources/test/de/monticore/lang/testsijava/testsijava/MyClass.sijava";
+        
+        TestSIJavaParser parser = new TestSIJavaParser();
+        Optional<ASTSIJavaClass> result = parser.parseSIJavaClass(model);
+        assertFalse(parser.hasErrors());
+        assertTrue(result.isPresent());
+        ASTSIJavaClass ast = result.get();
+    
+        TestSIJavaScopesGenitorDelegator genitor = TestSIJavaMill.scopesGenitorDelegator();
+        genitor.createFromAST(ast);
+        
+        String output = PrintAsJavaClass.printAsJavaClass(ast);
+    
+        ParserConfiguration configuration = new ParserConfiguration();
+        JavaParser parsers = new JavaParser(configuration);
+        ParseResult parseResult = parsers.parse(output);
+        Assert.assertTrue(parseResult.isSuccessful());
     }
 
     @Test
-    public void test2() {
-        String model = "test/de/monticore/lang/testsijava/testsijava/Main.sijava";
-        printAsJavaClass(model);
-    }
-
-    public void printAsJavaClass(String model) {
-        ASTSIJavaClass astsiJavaClass = parseModel(model);
-      TestSIJavaMill.scopesGenitorDelegator().createFromAST(astsiJavaClass);
-        String print = PrintAsJavaClass.printAsJavaClass(astsiJavaClass);
-        System.out.println(print);
-    }
-
-    private ASTSIJavaClass parseModel(String input) {
+    public void test2() throws IOException {
+        String model = "src/test/resources/test/de/monticore/lang/testsijava/testsijava/Main.sijava";
+        
         TestSIJavaParser parser = new TestSIJavaParser();
-        Optional<ASTSIJavaClass> res = Optional.empty();
-        try {
-            res = parser.parseSIJavaClass("src/test/resources/" + input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assertTrue(res.isPresent());
-        return res.get();
+        Optional<ASTSIJavaClass> result = parser.parseSIJavaClass(model);
+        assertFalse(parser.hasErrors());
+        assertTrue(result.isPresent());
+        ASTSIJavaClass ast = result.get();
+    
+        TestSIJavaScopesGenitorDelegator genitor = TestSIJavaMill.scopesGenitorDelegator();
+        genitor.createFromAST(ast);
+    
+        String output = PrintAsJavaClass.printAsJavaClass(ast);
+    
+        ParserConfiguration configuration = new ParserConfiguration();
+        JavaParser parsers = new JavaParser(configuration);
+        ParseResult parseResult = parsers.parse(output);
+        Assert.assertTrue(parseResult.isSuccessful());
     }
 }
